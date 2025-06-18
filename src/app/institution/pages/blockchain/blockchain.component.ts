1eb7cd634277bc35562d6cc0b100df0bbe2e75f3
ELÍAS TORRES ESPINOZA <pierotarazona633@gmail.com>
2025-06-17 22:10:40 -0500

    feat: implement institution folders

---
+++ src/app/institution/pages/blockchain/blockchain.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blockchain',
+  imports: [],
+  templateUrl: './blockchain.component.html',
+  styleUrl: './blockchain.component.css'
+})
+export class BlockchainComponent {
+
+}
---
+++ src/app/institution/pages/teachers/teachers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teachers',
+  imports: [],
+  templateUrl: './teachers.component.html',
+  styleUrl: './teachers.component.css'
+})
+export class TeachersComponent {
+
+}
